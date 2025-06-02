ce236a0e4d74669b9c7584bc99ad0aa1ed6afa62
Leila Hadj-Chikh <lhadjchikh@users.noreply.github.com>
2025-06-02 06:37:21 -0400

    fix(terraform): fix app security group rules

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -22,7 +22,23 @@ resource "aws_security_group" "app_sg" {
     description = "HTTPS from internet"
   }
 
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow traffic to container port"
+  }
+
   # Restricted egress rules
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP outbound traffic"
+  }
+
   egress {
     from_port   = 443
     to_port     = 443
@@ -32,11 +48,11 @@ resource "aws_security_group" "app_sg" {
   }
 
   egress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8000
+    to_port     = 8000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "HTTP outbound traffic"
+    description = "Outbound traffic on container port"
   }
 
   egress {
